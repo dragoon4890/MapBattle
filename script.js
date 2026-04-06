26b63e609576735cb813aa5053672c39a0c4dd69
dragoon4890 <harshsks123@gmail.com>
2026-04-06 09:05:34 +0530

    Added Leaflet js template, source cdn

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("Mahe Guessr starter template is ready!");
   });
 });
+13.131126789696053, 77.58778377525103
+
+var map = L.map('map', {
+    center: [13.131126789696053, 77.58778377525103],
+    zoom: 10
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
